2037546a7be575505860aaa9e422c0fd578dd8ca
tecblyu <105578017+tecblyu0@users.noreply.github.com>
2024-02-17 14:40:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var x = 10;
+var y = 39843 - 8374 / 12;
+
+console.log(x);
+console.log(y);
+
+console.log(y - x);
+
+// Primtive and Objects
+var original = {};
+
+original['Cov0'] = 'Y=T'
+
+console.log(original);
